b1b0706619cfe8cd056b0128f46baeb7f3263b8f
James Kessler <james.kessler@forallsecure.com>
2022-10-12 14:17:53 -0600

    fixup scan parameter usage

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker run -it \
-  -e MAPI_TOKEN=<< parameters.mapi-token >> \
-  -e GITHUB_TOKEN=<< parameters.github-token >> \
-  -e POSTMAN_ENVIRONMENT=<< parameters.postman-environment-id >> \
-  -e POSTMAN_API_KEY=<< parameters.postman-api-key >> \
+  -e MAPI_TOKEN=${<< parameters.mapi-token >>} \
+  -e GITHUB_TOKEN=${<< parameters.github-token >>} \
+  -e POSTMAN_ENVIRONMENT=${<< parameters.postman-environment-id >>} \
+  -e POSTMAN_API_KEY=${<< parameters.postman-api-key >>} \
   forallsecure/mapi:latest \
-  --url <<parameters.api-url >> \
-  << parameters.api-spec >>
+  --url ${<<parameters.api-url >>} \
+  ${<< parameters.api-spec >>}
