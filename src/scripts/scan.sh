c9e93561c7d6bbf4fa933d33b535c491456a8904
James Kessler <james.kessler@forallsecure.com>
2022-10-13 10:25:39 -0600

    download mapi instead of use docker

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-docker run -it \
-  --env MAPI_TOKEN \
-  --env GITHUB_TOKEN \
-  --env POSTMAN_ENVIRONMENT \
-  --env POSTMAN_API_KEY \
-  forallsecure/mapi:latest \
-  --url "${API_URL}" \
-  "${API_SPEC}"
+
+MAPI="${MAPI_PATH}/mapi"
+
+mkdir -p "${MAPI_PATH}"
+
+# Download executable
+curl -Lo "${MAPI}" "https://mayhem4api.forallsecure.com/downloads/cli/${MAPI_VERSION}/linux-musl/mapi" \
+  && chmod +x "${MAPI}"
+
+${MAPI} run --url "${API_URL}" "${TARGET}" "${DURATION}" "${API_SPEC}"
