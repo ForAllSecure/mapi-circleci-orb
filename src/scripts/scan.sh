9fde42aea0089a8ea93e38bae39ab8035df297c7
James Kessler <james.kessler@forallsecure.com>
2022-10-13 12:13:21 -0600

    add zap

--- src/scripts/scan.sh
+++ src/scripts/scan.sh
@@ -4,9 +4,16 @@
 export MAPI_TOKEN="${!MAPI_TOKEN_NAME}"
 export POSTMAN_API_KEY="${!POSTMAN_API_KEY_NAME}"
 export GITHUB_TOKEN="${!GITHUB_TOKEN_NAME}"
+
+# Default the target to the circle ci repo name
 TARGET="${TARGET:-$CIRCLE_PROJECT_REPONAME}"
 
 MAPI="${MAPI_PATH}/mapi"
+
+params=()
+[[ $ZAP_API_SCAN == 1 ]] && params+=(--zap)
+
+echo "${params[@]}"
 
 mkdir -p "${MAPI_PATH}"
 
@@ -14,4 +21,4 @@ mkdir -p "${MAPI_PATH}"
 curl -Lo "${MAPI}" "https://mayhem4api.forallsecure.com/downloads/cli/${MAPI_VERSION}/linux-musl/mapi" \
   && chmod +x "${MAPI}"
 
-${MAPI} run --url "${API_URL}" "${TARGET}" "${DURATION}" "${API_SPEC}"
+${MAPI} run "${params[@]}" --url "${API_URL}" "${TARGET}" "${DURATION}" "${API_SPEC}"
